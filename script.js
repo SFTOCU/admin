a6e7f4b8345073438cd6c12a38ae554b33b05380
NonomiyaYuta <ynonomiya.ocum@gmail.com>
2019-06-13 17:21:56 +0900

    支部員ツール作成

---
+++ script.js
@@ -0,0 +1,31 @@
+var smartView = false;
+var contentWidth = 100;
+var contentHeight = 100;
+window.onload = function () {
+    if(window.innerWidth<600){
+        smartView=true;
+        document.getElementById("app").classList.add("smartView");
+        contentWidth = Math.round(window.innerWidth/2)-30;
+        contentHeight = contentWidth;
+    }
+    myApp = new Vue({
+        el:"#app",
+        data:{
+            login:true,
+            liff:false,
+            userName:"リンゴ",
+            userId:"",
+            studentNumber:"",
+            password:"",
+            contentStyle:"width:"+contentWidth.toString()+";height:"+contentHeight.toString(),
+        },
+        methods:{
+            logIn:function(){
+                this.login = true;
+            },
+            logOut:function(){
+                this.login = false;   
+            },
+        }
+    });
+};
