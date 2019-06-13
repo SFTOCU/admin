42e3fa5ccd3516bc244cfb9387d720b907b9778b
NonomiyaYuta <ynonomiya.ocum@gmail.com>
2019-06-13 17:43:45 +0900

    支部員ツール作成

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var smartView = false;
 var contentWidth = 100;
 var contentHeight = 100;
+var device = "pc";
 window.onload = function () {
-    if(window.innerWidth<600){
+    var ua = navigator.userAgent;
+    if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+        device = "mobile";// スマートフォン用コード
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        device = "tablet";// タブレット用コード
+    }
+    if((device=="pc"&&window.innerWidth<600)||device!="pc"){
         smartView=true;
         document.getElementById("app").classList.add("smartView");
         contentWidth = Math.round(window.innerWidth/2)-30;
@@ -24,7 +31,7 @@ window.onload = function () {
                 this.login = true;
             },
             logOut:function(){
-                alert(window.innerWidth);
+                alert([device,window.innerWidth]);
                 this.login = false;   
             },
         }
